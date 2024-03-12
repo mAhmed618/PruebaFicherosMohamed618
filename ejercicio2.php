3d3bd6ea6673377997bd06061f13d4801e0131d5
mAhmed618 <618@cifpceuta.es>
2024-03-12 18:58:06 +0100

    php sin comentarios

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,18 +1,13 @@
 <?php
-// Nombre del archivo original
 $nombreArchivoOriginal = "fichero1.txt";
-// Nombres de los archivos de destino
 $archivoConNumeros = "f1.txt";
 $archivoSinNumeros = "f2.txt";
-// Abrir el archivo original para lectura
 $archivoOriginal = fopen($nombreArchivoOriginal, "r");
-// Abrir o crear los archivos de destino para escritura
+
 $f1 = fopen($archivoConNumeros, "w");
 $f2 = fopen($archivoSinNumeros, "w");
-// Verificar si el archivo se abrió correctamente
 if ($archivoOriginal !== false) {
     while (($linea = fgets($archivoOriginal)) !== false) {
-        // Comprobar si la línea contiene un número y escribirla en su archivo
         if (preg_match('/\d/', $linea)) {
             
             fwrite($f1, $linea);
@@ -21,7 +16,6 @@ if ($archivoOriginal !== false) {
             fwrite($f2, $linea);
         }
     }
-    // Cerrar los archivo s
     fclose($archivoOriginal);
     fclose($f1);
     fclose($f2);
